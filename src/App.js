9d6e94a378441cdbbbaad6156be058066e9c8e21
Jacob Fernandes <59456400+proballstar@users.noreply.github.com>
2021-09-04 22:40:08 -0700

    Adding Main Functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine } from "react-chat-engine";
 
-import ChatFeed from './components/ChatFeed';
-import LoginForm from './components/LoginForm';
-import './App.css';
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = "12046f51-60e7-4623-8297-a8180e0e28fb";
 
 const App = () => {
-  if (!localStorage.getItem('username')) return <LoginForm />;
+  if (!localStorage.getItem("username")) return <LoginForm />;
 
   return (
     <ChatEngine
       height="100vh"
       projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
